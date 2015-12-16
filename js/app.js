0a7240c1bce7a0c492ff5c7e65a26b2c6777afe1
Gio Lapid <giolapid@gmail.com>
2015-12-15 21:46:06 -0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,40 @@
+//Problem User interaction doesnt work
+//solution add interactivity
+
+//Add a new task
+	//when button is pressed
+	//create a new list iteam with the test from #new-task
+		//input checkpox
+		//label
+		//input text
+		//button.edit
+		//button.delete
+		//each elements, needs modified and appended
+
+//Edit Existing Task
+	//when edit button is pressed
+  		//if the class of the parent is .editMode
+  		  //switch from .editMode
+  		  //label text become the input's value
+  		//else
+  		  //switch to editMode
+  		  //input value becomes the labels text
+
+  		//toggle .editMode
+
+//Delete existing task
+	//when delete button is pressed
+		//remove parent list item from the ul
+
+
+//mark task as complete
+	//when the checkbox is checked
+	  //append the task list item to the completed-tasks
+
+
+
+
+//mark task as incomplete
+
+  	//when checkbox is unchecked
+  	  //append the task list item to incomplete task
